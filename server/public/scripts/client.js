0d75be508fea794e5a7923a63cb8d5d2eece4d19
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-29 11:08:45 -0500

    wont push random number object value

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,87 @@
-$(document).ready(handleReady);
 
+$(document).ready(handleReady);
+let rando;
+
+let round=1;
 function handleReady() {
   console.log("jquery is loaded!")
+
+  //register on submit
+  // getRandomNumbFromServer();
+  $('#inputForm').on('submit', onSubmitGuess);
+}
+
+function getRandomNumbFromServer(){
+  $.ajax({
+    url: '/getRandomNum',
+    method: 'GET'
+  })
+  .then((response)=>{
+    console.log('in ajax /getRandomNum then');
+    rando = response.stateRandomNumber;
+    console.log('rando is',rando);
+  })
+  .catch((err)=>{
+    console.log('/getRandomNum err',err);
+  }); 
+};
+
+function onSubmitGuess(evt){
+  evt.preventDefault();
+  rando = getRandomNumbFromServer()
+  console.log('in onSubmitGuess 🐈');
+  //object creation to send to server
+  let p1Choice=$('#inputPlayer1').val();
+  let p2Choice=$('#inputPlayer2').val();
+  let p1Result=whoWins($('#inputPlayer1').val());
+  let p2Result=whoWins($('#inputPlayer2').val());
+  let guesses={
+    round:round++,
+    Number: rando,
+    p1:p1Choice,
+    p2: p2Choice,
+    resultP1: p1Result,
+    resultP2: p2Result
+    };
+  
+  $.ajax({
+    url: '/getHistory',
+    method: 'POST',
+    data: guesses
+  })
+    .then((response)=> {
+      console.log('/getHistory POST');
+      // if(response==='Created'){
+
+      // }
+    })
+    .catch((err)=>{
+      console.log('/getHistory err', err);
+    });
+
+
+}
+
+function whoWins(player){
+  console.log('in whoWins');
+
+  //check if p1 or p2 wins or if they are too low or high
+  
+  if(player===rando){
+    
+    return 'WIN! 🔥'
+  }
+  else if(player>rando){
+    return 'Too High 😦'
+  }
+  else{
+    return 'Too Low 😦'
+  }
+}
+
+
+
+function render(){
+
+
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 3000;
 
+let stateRandomNumber ;
+let stateHistory =[]
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,8 +12,29 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.get('/getRandomNum',(req,res)=>{
+  // console.log('in getRandomNum');
+  // stateRandomNumber=getRandomNumber();
+  stateRandomNumber = (Math.ceil(Math.random()*25));
+  // console.log('RandomNumber is',stateRandomNumber);
+  res.send({stateRandomNumber});
+});
 
+
+
+app.post('/getHistory', (req, res)=>{
+  console.log('in /getHistory POST');
+  stateHistory.push(req.body);
+  // stateRandomNumber=getRandomNumber();
+  console.log(stateHistory);
+  res.send('201');
+});
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
-})
+});
+
+
+// function getRandomNumber(){
+//   return (Math.ceil(Math.random()*25));
+// }
